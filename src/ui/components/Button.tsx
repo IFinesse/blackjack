3a9b11e6291f054c9a3daee4da92abb76fc7932b
Ivan Shitikov <ifinesse2406@gmail.com>
2022-08-19 18:57:25 +0300

    started making game screen

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -4,8 +4,8 @@ import {Dimensions, StyleSheet, Text, TouchableOpacity} from 'react-native';
 interface Props {
   onPress: () => void;
   text: string;
-  buttonStyle: any;
-  textStyle: any;
+  buttonStyle?: any;
+  textStyle?: any;
 }
 
 const Button: FC<Props> = ({onPress, text, buttonStyle, textStyle}) => {
@@ -21,12 +21,12 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width / 1.1,
     height: 50,
     borderWidth: 1,
-    borderRadius: 10,
+    borderRadius: 5,
     alignSelf: 'center',
     backgroundColor: '#AAFF33',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 75,
+    // marginTop: 75,
   },
   text: {},
 });
--- src/ui/screens/Game/GameScreen.tsx
+++ src/ui/screens/Game/GameScreen.tsx
@@ -1,14 +1,20 @@
 import React, {FC, useEffect, useState} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  Pressable,
+  Dimensions,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import {logOut} from '../../../store/auth/authSlice';
 
 import {useAppDispatch} from '../../../store/hooks';
 
-function startGame() {
-  const game = {};
-  game['randomDeck'] = [...deck].sort((a, b) => 0.5 - Math.random());
-}
+import Button from '../../components/Button';
 
 const GameScreen: FC = ({}) => {
   const dispatch = useAppDispatch();
@@ -112,23 +118,81 @@ const GameScreen: FC = ({}) => {
   }
 
   return (
-    <View style={styles.container}>
-      {/* <Text>Home</Text> */}
-      <View style={styles.header}>
-        <Text>icon</Text>
-        <TouchableOpacity onPress={onLogOut}>
-          <Text>log out</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.main}>
-        <Text>dealer cards: </Text>
-        <Text>{dealerCards[0]} X</Text>
-        <Text>your cards: </Text>
-        <Text>{playerCards}</Text>
-        <Text>the winner:</Text>
-        {/* <Text>{calculateWinner(dealerCards, playerCards)}</Text> */}
-      </View>
-    </View>
+    <LinearGradient
+      colors={['#001F00', '#003F00']}
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 0}}
+      locations={[0, 0.8]}
+      style={styles.container}>
+      <View style={styles.main}></View>
+      <ImageBackground
+        source={require('../../../../assets/wood1.jpeg')}
+        resizeMode="cover"
+        style={styles.bottomContainer}>
+        <Pressable onPress={() => {}} style={styles.amountContainer}>
+          <View style={styles.amount}>
+            <Text style={styles.amountText}>100000</Text>
+          </View>
+          <View style={styles.plusContainer}>
+            <LinearGradient
+              colors={['#FF7F10', '#FFD700', '#FF7F10']}
+              start={{x: 0, y: 0}}
+              end={{x: 1, y: 1}}
+              style={styles.plus}>
+              <Text style={{fontSize: 19, padding: 0}}>+</Text>
+            </LinearGradient>
+            <View style={styles.plusNotification}>
+              <Text style={styles.plusText}>1</Text>
+            </View>
+          </View>
+        </Pressable>
+        <View style={styles.chipsContainer}>
+          <Pressable style={styles.chip}>
+            <Text>1</Text>
+          </Pressable>
+          <Pressable style={styles.chip}>
+            <Text>5</Text>
+          </Pressable>
+          <Pressable style={styles.chip}>
+            <Text>10</Text>
+          </Pressable>
+          <Pressable style={styles.chip}>
+            <Text>25</Text>
+          </Pressable>
+          <Pressable style={styles.chip}>
+            <Text>100</Text>
+          </Pressable>
+        </View>
+        <View style={styles.buttonsContainer}>
+          <Button
+            onPress={() => {}}
+            text="Max. bet"
+            buttonStyle={{
+              width: Dimensions.get('window').width / 3.5,
+              height: 20,
+            }}
+          />
+          <Button
+            onPress={() => {}}
+            text="Deal"
+            buttonStyle={{
+              width: Dimensions.get('window').width / 3.5,
+              height: 20,
+            }}
+          />
+          <Button
+            onPress={() => {}}
+            text="Max. bet1"
+            buttonStyle={{
+              width: Dimensions.get('window').width / 3.5,
+              height: 20,
+              position: 'relative',
+              top: 100,
+            }}
+          />
+        </View>
+      </ImageBackground>
+    </LinearGradient>
   );
 };
 
@@ -147,8 +211,92 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   main: {
-    flex: 8,
-    backgroundColor: '#ffd',
+    flex: 5,
+    // backgroundColor: '#ffd',
+  },
+  bottomContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  amountContainer: {
+    flexDirection: 'row',
+  },
+  amount: {
+    backgroundColor: 'rgba(0,0,0, 0.3)',
+    borderRadius: 4,
+    shadowOffset: {
+      width: 0,
+      height: -1,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 6,
+    shadowColor: '#000',
+  },
+  amountText: {
+    fontSize: 13,
+    color: '#FFF',
+    fontWeight: '500',
+    paddingHorizontal: 25,
+    paddingVertical: 3,
+  },
+  plusContainer: {
+    position: 'relative',
+    left: 10,
+  },
+  plus: {
+    width: 24,
+    height: 24,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  plusText: {
+    color: 'white',
+    fontSize: 9,
+    fontFamily: 'SourceCodePro-Bold',
+    position: 'relative',
+    // left: 1,
+    top: -1,
+  },
+  plusNotification: {
+    position: 'absolute',
+    width: 12,
+    height: 12,
+    borderRadius: 5,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    left: 18,
+    top: -6,
+  },
+  chipsContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'rgba(0,0,0, 0.3)',
+    borderRadius: 15,
+    shadowOffset: {
+      width: 0,
+      height: -1,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 6,
+    shadowColor: '#000',
+  },
+  chip: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    borderWidth: 5,
+    borderColor: 'red',
+    backgroundColor: 'white',
+    marginHorizontal: 5,
+  },
+  buttonsContainer: {
+    width: Dimensions.get('window').width / 1.1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
